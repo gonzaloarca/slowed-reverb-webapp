de18802b5d66baadc37d03b3f9e03460a56abba4
Gonzalo Arca <goarca@itba.edu.ar>
2023-02-12 02:09:38 -0300

    CSS fixes

--- src/layout/AppLayout/AppLayout.js
+++ src/layout/AppLayout/AppLayout.js
@@ -14,7 +14,7 @@ const AppLayout = () => {
 	const { createToneContext } = useContext(PlayerContext);
 
 	return (
-		<Layout className="h-screen" onClick={createToneContext}>
+		<Layout className={style.layout} onClick={createToneContext}>
 			<Header
 				className={style.header}
 				style={{
